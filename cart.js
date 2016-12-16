1d9ffb41fd0ddc777550f5738725797aaf8acebc
MatthewJCifue99 <7mcifuentes@stpatrick.org>
2016-12-16 10:51:37 -0600

    Add files via upload

---
+++ cart.js
@@ -0,0 +1,24 @@
+class Cart{
+  //What is the first part of every class? Type it below.
+contructor(itemList,itemQuantity){
+  this.itemList = il;
+  this.itemQuantity = iq;
+}
+
+
+  //Type the instance functions below this comment.
+addItem(i,q){
+  this.itemList.push(i);
+  this.itemQuantity.push(q);
+}
+
+totalCart(){
+  let total = 0
+  for(var c=0;c < this.itemList[].length;c++)
+  total = total+this.itemList[c].price.this.itemQuantity[c];
+}
+return total;
+}
+addCart(days){
+  let subChart
+}
---
+++ item.js
@@ -0,0 +1,18 @@
+class Item{
+  //Fill in the item class below this comment.
+  constructor (n ,p, s){
+    this.name= n;
+    this.price= p;
+    this.shipping= s;
+  }
+
+
+
+}
+
+//Create your three test items below this comment.
+let baseballequipment("demarinibat"249,6);
+let game("skyrim"60,5);
+let tv("samsungcurved"1800,7);
+
+console.log(baseballequipment);
